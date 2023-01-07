514f2eeb535736ade7c34cb044c61575a78d1598
Megan <esler6724@gmail.com>
2023-01-06 17:19:08 -0800

    commit tree c03bf81c631582560e975d0121b60d9fc4b4cd8f

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution here
+const tutorials = [
+    "what does the this keyword mean?",
+    "What is the Constructor OO pattern?",
+    "implementing Blockchain Web API",
+    "The Test Driven Development Workflow",
+    "What is NaN and how Can we Check for it",
+    "What is the difference between stopPropagation and preventDefault?",
+    "Immutable State and Pure Functions",
+    "what is the difference between == and ===?",
+    "what is the difference between event capturing and bubbling?",
+    "what is JSONP?",
+  ];
+
+  function titleCased(str) {
+    return str.toLowerCase().split(` `).map(function(word) {
+      return (word.chartAt(0).toUpperCase() + word.slice(1));
+    }).join(` `);
+  }
+  titleCased(tutorials)
+
+
+function findMatching(drivers, name) {
+  const filteredDrivers = []
+  for(let driver of drivers) {
+    if(driver.toLowerCase() === name.toLowerCase()) {
+      filteredDrivers.push(driver)
+    }
+  }
+  return filteredDrivers
+}
+
+function fuzzyMatch(drivers, letters) {
+  const fuzzyFilter = []
+  for(let driver of drivers) {
+    if(driver.startsWith(letters)) {
+      fuzzyFilter.push(driver)
+    }
+  }
+  return fuzzyFilter
+}
+
+
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name === string)
+}
